607f064c6f7e8e52475a37ab98aaddb93d929824
Vanishree Yarakad <vanishree.y@BRILLIO.COM>
2018-07-19 16:35:42 +0530

    deleted the code

--- src/main/scala/com/jemstep/bulkapi/v2/CreateJobRequest.scala
+++
@@ -1,8 +0,0 @@
-package com.jemstep.bulkapi.v2
-
-import scala.beans.{BeanProperty}
-
-class CreateJobRequest(@BeanProperty val `object`: String,
-                       @BeanProperty val contentType: String,
-                       @BeanProperty val operation: String,
-                       @BeanProperty val lineEnding: String)
--- src/main/scala/com/jemstep/model/BusinessModel.scala
+++
@@ -1,9 +0,0 @@
-package com.jemstep.model
-
-import com.jemstep.model.BusinessEntityModel.EntityModel
-
-trait BusinessModel {
-
-  def extractEntityModels(userId: String, org: String, operation: String): List[EntityModel]
-
-}
